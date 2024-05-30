a527e66aefaf7208d6004f005f98b7dbc1bd1e21
Oscar Silva <oscarsilva2356@gmail.com>
2024-05-29 22:40:49 -0700

    Added more test

--- src/test/java/org/example/AddNumTest.java
+++ src/test/java/org/example/AddNumTest.java
@@ -1,13 +1,41 @@
 package org.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AddNumTest {
-    AddNum num = new AddNum();
-@Test
-    public void itShouldEqualsToFour() {
-    assertEquals(4, num.addTwoNum(2, 2));
+    AddNum num;
+
+    @BeforeAll
+    public static void setUpAll() {
+        System.out.println("Should Print Before All Tests. (This is awesome!)");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        num = new AddNum();
+    }
+
+    @Test
+    @DisplayName("Should Equal to Four")
+    public void itShouldEqualToFour() {
+        Assertions.assertEquals(4, num.addTwoNum(2, 2));
 }
+    @Test
+    @DisplayName("Should Equal to Seven")
+    public void itShouldEqualToSeven() {
+        Assertions.assertEquals(7, num.addTwoNum(3,4));
+    }
+
+    @Test
+    @DisplayName("Should equal to Twelve")
+    public void itShouldEqualToTwelve() {
+        Assertions.assertEquals(12, num.productFourNum(3,4));
+    }
+
+    @AfterAll
+    public static void setDownAll() {
+        System.out.println("Should Print Once All Test Are Done.");
+    }
 }
